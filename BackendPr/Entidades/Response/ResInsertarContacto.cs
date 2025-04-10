7096b34d229e5ead994f28d72d859066378b72b1
OVIEDO CERDAS <jeziel.oviedo@ulatina.net>
2025-04-10 03:51:46 -0600

    Creo que contacto funciona, error en errorDes

---
+++ BackendPr/Entidades/Response/ResInsertarContacto.cs
@@ -0,0 +1,17 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendPr.Entidades
+{
+    public class ResInsertarContacto : ResBase
+    {
+       
+        public bool Resultado { get; set; }
+
+       
+    }
+}
